00990770ac2fdb07b31f436f321f8fcf5a63f09e
Kamila Duda <kakus86@o2.pl>
2021-01-22 11:22:22 +0100

    Components implemented

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class="navbar__container">shop</nav>
+    <ProductCard />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProductCard from "./components/ProductCard.vue"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ProductCard,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.navbar__container{
+  background-color: #9e9d9d;
 }
 </style>
