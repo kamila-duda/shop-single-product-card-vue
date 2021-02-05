f79fc64f65a3877b813b3a02f825cd8fc85ff96e
Kamila Duda <kakus86@o2.pl>
2021-02-05 10:02:37 +0100

    added side page without interaction

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@
     <Navigation 
     v-bind:inCart = "inCart"
     />
+    <SidePage/>
     <ProductCard @inShoppingCart="inShoppingCart" />
   </div>
 </template>
 
 <script>
 import ProductCard from "./components/ProductCard/ProductCard.vue"
-import Navigation from "./components/Navigation.vue"
+import Navigation from "./components/Navigation.vue";
+import SidePage from "./components/SidePage/SidePage.vue"
 export default {
   name: 'App',
   components: {
     Navigation,
     ProductCard,
+    SidePage,
   },
   data(){
     return{
---
+++ src/components/SidePage/SidePage.vue
@@ -0,0 +1,38 @@
+<template>
+  <aside class="sidePage__container">
+    <details open>
+      <summary class="sidePage__detailsHeader">Kruszec</summary>
+      <p>Srebro (2)</p>
+      <p>Białe złoto (3)</p>
+      <p>Złoto (5)</p>
+    </details>
+    <details>
+      <summary class="sidePage__detailsHeader">Rodzaj</summary>
+      <p>Łańcuszki (2)</p>
+      <p>Pierścionki (3)</p>
+      <p>Kolczyki (5)</p>
+    </details>
+  </aside>
+</template>
+
+<script>
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.sidePage__container {
+  position: absolute;
+  min-height: 80vh;
+  width: 15%;
+top: 100px;
+padding: 25px;
+}
+.sidePage__detailsHeader{
+    cursor: pointer;
+    background-color: black;
+    color: white;
+    padding: 5px 10px;
+    border-radius: 15px;
+    margin-top: 8px;
+}
+</style>
